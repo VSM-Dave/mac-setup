f529fa83454f7fc64613afeeba46402fc1b21ecf
David <davie668@gmail.com>
2021-02-21 14:55:14 +0000

    Change Rails New command to Jumpstart New

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -12,7 +12,7 @@ cp dotfiles/gitattributes ~/.gitattributes
 cp dotfiles/gitconfig ~/.gitconfig
 cp dotfiles/gitignore ~/.gitignore
 cp dotfiles/ruby_version ~/.ruby_version
-cp dotfiles/zsh_functions/rails_new ~/.zsh_functions/rails_new
+cp dotfiles/zsh_functions/jumpstart_new ~/.zsh_functions/jumpstart_new
 cp dotfiles/zsh_functions/clean_code ~/.zsh_functions/clean_code
 cp dotfiles/zsh_functions/update_project ~/.zsh_functions/update_project
 cp dotfiles/zshrc ~/.zshrc
